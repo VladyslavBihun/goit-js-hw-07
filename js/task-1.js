172bd6b376fd40062a3251331df7e7e9c8bd9cdf
VladyslavBihun <bihunvv@gmail.com>
2024-03-10 19:27:38 +0200

    1 task completed

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+// const list = document.querySelector("#categories")
+// console.log(list);
+
+// const items = document.querySelectorAll(".item")
+// console.log(items);
+
+const categories = document.querySelectorAll('#categories .item');
+
+  console.log(`Number of categories: ${categories.length}`);
+
+  categories.forEach(function(category) {
+    const categoryName = category.querySelector('h2').textContent;
+    const items = category.querySelectorAll('li');
+
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${items.length}`);
+  });
